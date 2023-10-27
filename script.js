0d72811dfcb46b81e39521e8717ec10643e3947f
KOSX000 <66070076@kmitl.ac.th>
2023-10-27 16:22:07 +0700

    init

---
+++ script.js
@@ -0,0 +1,10 @@
+function setName() {
+    const nameField = document.getElementById("username");
+    const name = (nameField.value);
+    const title = document.getElementById("profile-link");
+    title.innerText = name + " Profile"
+}
+
+function setImage() {
+    const profileimgae
+}
